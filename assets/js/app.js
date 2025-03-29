20785d8b4101aefb6f9fdffb9c708099f2796d4a
VincinChristmas <VincinChristmas@hotmail.com>
2025-03-29 17:29:28 -0500

    added new javascript code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,17 @@
-let emailInput = document.getElementById('email')[0];
 
-function emailValid(email) {
-    return /\S+@\S+\.\S+/.test(email);
-}
+    let emailModal = document.getElementsByClassName("email-modal")[0];
+    let emailInput = document.getElementsByClassName("email-modal__input")[0];
+
+    function emailValid(email) {
+        return /\S+@\S+\.\S+/.test(email);
+      }
+
+      let addErrors = () => {
+        document
+          .getElementsByClassName("container__form--left--form--input")[0]
+          .classList.add("email-modal__form-group--error");
+        document
+          .getElementsByClassName("email-modal__error-message")[0]
+          .classList.add("email-modal__error-message--active");
+      };
+     

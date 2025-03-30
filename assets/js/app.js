a86faca34bcbf7482dbe210267156f9954e6f397
VincinChristmas <VincinChristmas@hotmail.com>
2025-03-30 16:46:52 -0500

    added email modal partially works

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,8 +3,21 @@ document.addEventListener('DOMContentLoaded', function() {
     let emailInput = document.getElementsByClassName("email-modal__input")[0];
     let emailButton = document.getElementsByClassName("email-modal__button")[0];
     let emailModal = document.getElementsByClassName("email-modal")[0];
-    let leftContainer = document.getElementsByClassName("container__form--left")[0];
-    let rightContainer = document.getElementsByClassName("container__form--right")[0];
+    let containerBlock = document.getElementsByClassName("container")[0];
+    let emailNotValid = document.getElementsByClassName("invalidEmail")[0];
+
+    /*
+    let closeInput = () => {
+        emailInput.classList.remove("email-modal__input");
+      };
+
+      closeInput();
+
+      let openInput = () => {
+        emailInput.classList.add("email-modal__input");
+      };
+      */
+    
 
     function emailValid(email) {
         return /\S+@\S+\.\S+/.test(email);
@@ -14,18 +27,22 @@ document.addEventListener('DOMContentLoaded', function() {
         e.preventDefault();
       });
 
-
+      
+      let openModal = () => {
+        emailModal.style.display = "block";
+      };
       
 
       emailButton.addEventListener("click", () => {
         if (emailValid(emailInput.value)) {
-            emailModal.style.display = "block";
-            leftContainer.style.display = "none";
-            rightContainer.style.display = "none";
-
+            setTimeout(() => {
+                openModal();
+              }, 200);
+            containerBlock.style.display = "none";
             
           } else {
-            console.log("Please provide a valid email address");
+            emailNotValid.style.display = "block";
+           /* openInput(); */
           }
       });
 
